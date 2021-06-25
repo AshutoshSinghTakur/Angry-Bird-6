e800421c95cc5a7659d8139f99c10c6f15b0019c
AshutoshSinghTakur <ashuthakur1718@gmail.com>
2021-06-25 16:46:59 +0530

    new slingshot

--- Slingshot.js
+++ Slingshot.js
@@ -8,6 +8,9 @@ class SlingShot{
         }
         this.pointB = pointB
         this.sling = Constraint.create(options);
+        this.Img1 = loadImage("sprites/sling1.png");
+        this.Img2 = loadImage("sprites/sling2.png");
+        this.Img3 = loadImage("sprites/sling3.png");
         World.add(world, this.sling);
     }
 
@@ -19,9 +22,23 @@ class SlingShot{
         if(this.sling.bodyA){
             var pointA = this.sling.bodyA.position;
             var pointB = this.pointB;
-            strokeWeight(4);
-            line(pointA.x, pointA.y, pointB.x, pointB.y);
+            image(this.Img3,pointA.x-30,pointA.y-10,15,30);
+            if(pointA.x<220 && pointA.x>100){
+                strokeWeight(10);
+                stroke(48,22,10);
+                line(pointA.x-20, pointA.y, pointB.x-10, pointB.y);
+                line(pointA.x-20,pointA.y,pointB.x+30,pointB.y-3)
+          }else{
+              strokeWeight(3)
+              stroke(48,22,10);
+              line(pointA.x+25, pointA.y, pointB.x-10, pointB.y);
+              line(pointA.x+25,pointA.y,pointB.x+30,pointB.y-3)
+          }
         }
+
+        image(this.Img1,200,20)
+        image(this.Img2,170,20)
+
     }
-    
+   
 }
